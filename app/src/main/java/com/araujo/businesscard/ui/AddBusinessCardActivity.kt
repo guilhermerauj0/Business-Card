c5527a13933d9d80d8ce00925840e7be31d48b76
arthu <arthurguilhermesb@gmail.com>
2022-06-18 14:27:02 -0300

    Configure Add Acitivity for add cards and fix bugs

--- app/src/main/java/com/araujo/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/araujo/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,11 +2,20 @@ package com.araujo.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.araujo.businesscard.App
+import com.araujo.businesscard.R
+import com.araujo.businesscard.data.BusinessCard
 import com.araujo.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel : MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +30,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.btnConfirm.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                name = binding.tilName.editText?.text.toString(),
+                enterprise = binding.tilEnterprise.editText?.text.toString(),
+                phone = binding.tilPhone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                background = binding.tilColor.editText?.text.toString()
+            )
+            mainViewModel.inser(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
     }
 }
