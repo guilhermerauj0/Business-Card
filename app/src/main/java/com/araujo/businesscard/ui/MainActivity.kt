7cb16c289177df47f05121d228700e677d65e761
arthu <arthurguilhermesb@gmail.com>
2022-06-17 22:40:57 -0300

    Add dependecies of room database and lifecycle. Add viewBinding

--- app/src/main/java/com/araujo/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/araujo/businesscard/ui/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.araujo.businesscard
+package com.araujo.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.araujo.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
